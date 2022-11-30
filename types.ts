fe94c6fe42249c84bcb99876c34a40619501bf5b
Camila Rondinini <crondinini@gmail.com>
2022-11-30 13:37:06 +0100

    another change

--- types.ts
+++ types.ts
@@ -1,7 +1,7 @@
 import { DefaultCtx, SessionContext, SimpleRolesIsAuthorized } from "blitz"
 import { User } from "db"
 
-// Note: You should switch to Postgres and then use a DB enum for role type
+// Note: You should switch to Postgres and then use a DB enum for role type.
 export type Role = "ADMIN" | "USER"
 
 declare module "blitz" {
